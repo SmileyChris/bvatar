f8b7e982f4ca90b35f42bfc1b8b68ed61b4b190c
Chris Beaven <smileychris@gmail.com>
2014-09-11 15:17:11 +1200

    Add a setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='bvatar',
+    version='1.a1',
+    url='https://github.com/smileychris/bvatar',
+    #download_url='',
+    description='Random-art avatar generation',
+    license='BSD',
+    long_description=open('README.rst').read(),
+    author='Chris Beaven',
+    author_email='smileychris@gmail.com',
+    platforms=['any'],
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'bvatar = bvatar:main',
+        ],
+    },
+    install_requires=['six', 'docopt', 'bitarray'],
+    extras_require = {
+        'bitmap': ['pillow'],
+    },
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Multimedia :: Graphics',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
